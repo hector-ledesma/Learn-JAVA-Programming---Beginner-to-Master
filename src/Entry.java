60f2d038623f1779de562c78ecdb1bc71786a8bc
Hector <hectoremil21@hotmail.com>
2022-01-25 13:13:05 -0500

    Student Challenge: Quadratic Equation

--- src/Entry.java
+++ src/Entry.java
@@ -34,7 +34,23 @@ public class Entry {
         System.out.println("Area of the triangle is: " + area);
     }
 
+    static private void quadratic() {
+        int a, b, c;
+        double r1, r2;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a, b, c: ");
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+
+        r1 = (-b + Math.sqrt((b*b) - 4*a*c)) / (2*a);
+        System.out.println("r1 = " + r1);
+        r2 = (-b - Math.sqrt((b*b) - 4*a*c)) / (2*a);
+        System.out.println("r2 = " + r2);
+    }
+
     public static void main (String[] args) {
-        areaSides();
+        quadratic();
     }
 }
